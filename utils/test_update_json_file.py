6c8545b3fdf70d74e228a232577755cecc7f782b
mjkrause <michael.j.krause@gmail.com>
2018-05-14 15:57:47 -0700

    * added command line argument passing * some tests don't work yet * function does work

--- utils/test_update_json_file.py
+++ utils/test_update_json_file.py
@@ -5,7 +5,16 @@ import json
 import tempfile
 import shutil
 import os
-from utils.update_json_file import update_json_file
+import argparse
+from utils.update_json_file import main, parse_args
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    #parser.add_argument...
+    # ...Create your parser as you like...
+    return parser
+
 
 class TestUpdateJsonFile(unittest.TestCase):
 
@@ -20,8 +29,19 @@ class TestUpdateJsonFile(unittest.TestCase):
         with open(os.path.join(self.json_path, 'json_data.json'), 'w') as fp:
             json.dump(d, fp)
 
+        self.parser = create_parser()
+
     def tearDown(self):
         shutil.rmtree(self.json_path)
+
+    def test_parser(self):
+        parser = parse_args(['-l', '-m', '-s'])
+        #            ['input.json', 'old_path', 'new_path'])
+        self.assertTrue(parser.long)
+
+    def test_something(self):
+        parsed = self.parser.parse_args(['--something', 'test'])
+        self.assertEqual(parsed.something, 'test')
 
     def test_check_test_data(self):
         with open(os.path.join(self.json_path, 'json_data.json'), 'r') as fp:
@@ -30,22 +50,19 @@ class TestUpdateJsonFile(unittest.TestCase):
         self.assertEqual(data, {'key1': '/home/user/old_path/file1',
                                 'key2': '/home/user/old_path/file2',
                                 'key3': '/home/user/old_path/file3'}
-)
+                         )
 
     def test_update_json_file(self):
 
-        new_path = '/home/user/new_path/'
 
-        update_json_file(
+        main(
             os.path.join(os.path.join(self.json_path, 'json_data.json')),
-                                      '/home/user/old_path/',
-                                      new_path)
+            '/home/user/old_path/',
+            '/home/user/new_path')
         with open(os.path.join(self.json_path, 'json_data.json'), 'r') as fp:
             data = json.load(fp)
 
         print(data)
-
-
 
 
 if __name__=='__main__':
